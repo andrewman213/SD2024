f926f8abcf7a4d7af272b4c074a05e176773d634
andrewman213 <45531267+andrewman213@users.noreply.github.com>
2024-04-03 12:46:59 +0200

    Revert "FULL LOGIN SYSTEM"
    This reverts commit 34367d9a6510ba3214b1794cf10c28498d7f90a6.

--- registration.php
+++ registration.php
@@ -35,9 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           $insert->execute();
           
           if ($insert->affected_rows > 0) {
-              // Redirect to login page after successful registration
-              header("Location: login.php");
-              exit();
+              echo "New record created successfully";
           } else {
               echo "Error: " . $insert->error;
           }
